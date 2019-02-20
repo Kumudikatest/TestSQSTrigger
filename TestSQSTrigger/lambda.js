24c1fa18f990eeee51eb7388043022f19cc62df2
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-02-20 16:16:42 +0530

    trigger sqs

--- TestSQSTrigger/lambda.js
+++ TestSQSTrigger/lambda.js
@@ -1,7 +1,12 @@
 exports.handler = function (event, context, callback) {
+
+    event.Records.forEach(({ messageId, body }) => {
+        console.log('SQS message %s: %j', messageId, body);
+    });
+    return `Successfully processed ${event.Records.length} messages.`;
     
-    callback(null, { "message": "Successfully executed" });
-}
+};
+
 
 
 
